5c1be4053a13b3ffa92fef053f988935f2fe8cc3
Thiru P <123931175+ThiruNithish28@users.noreply.github.com>
2025-05-11 22:56:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/", // Change to "/repo-name/" if deploying to GitHub Pages
+  build: {
+    outDir: "dist", // Vite's default (must match Vercel settings)
+  }
 });
